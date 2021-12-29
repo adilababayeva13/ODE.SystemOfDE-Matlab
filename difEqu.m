244c63d841502fb904bafeef8a4778f259383f78
adilababyeva13 <adilababayeva13@gmail.com>
2021-12-30 02:58:10 +0400

    solution for repeated roots

--- difEqu.m
+++ difEqu.m
@@ -9,17 +9,33 @@ A = [ x1 y1 ; x2 y2];
 eigs = eig(A);
 fprintf('A matrix is :\n ');
 disp(A);
-fprintf('Eigenvalues of A are :\n ');
-disp(eigs);
+I=eye(2);
 %eigenvalues
-e1=eigs(1);
-e2=eigs(2);
-
+e1=eigs(1,1);
+e2=eigs(2,1);
+disp(" first eigenvalue is : ");
+disp(e1);
+disp(" second eigenvalue is : ");
+disp(e2);
 %checking cases
 
 if isreal(e1)
-   % 1) distinct real numbers
-    if e1 ~= e2
+   % 1) Repeated roots  
+    if abs(e1-e2) < 0.0001 % because they're floating point, we need to check against a tolerance
+       disp("repeated");
+       [V,D] = eig(A);
+       A2 = A - e1*I;
+       V1=[V(1,1) ; V(2,1)];
+       V2 = linsolve(A2,V1); % because A2*V2=V1
+       disp("vector 1 : " );
+       disp(V1);
+       disp("vector 2 : " );
+       disp(V2);
+       fprintf(" Solution is : " );
+       fprintf('C1(%f ; %f)e^%.ft + C2[(%f ; %f )te^%.ft + (%f ; %f )e^%.ft',V1(1,1),V1(2,1) , e1 ,V1(1,1),V1(2,1) , e1 ,V2(1,1),V2(2,1) , e1);
+ 
+    % 2) distinct real roots
+    else
        [V,D] = eig(A);
        V1=[V(1,1) ; V(2,1)];
        V2=[V(1,2) ; V(2,2)];
@@ -28,6 +44,6 @@ if isreal(e1)
        disp("vector 2 : " );
        disp(V2);
        fprintf(" Solution is : " );
-       fprintf('C1[%f ; %f]e^%.ft + C2[%f ; %f ]e^%.ft',V1(1,1),V1(2,1) , e1 ,V2(1,1),V2(2,1) , e2);
+       fprintf('C1(%f ; %f)e^%.ft + C2(%f ; %f )e^%.ft',V1(1,1),V1(2,1) , e1 ,V2(1,1),V2(2,1) , e2);
     end
 end
